b2278ccead52ee6befa5f8d5f5dbcb41efb01c5d
mraidi achref <mradiachref@gmail.com>
2024-05-09 00:26:37 +0100

    prosit12

--- src/StudentManagement.java
+++ src/StudentManagement.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
@@ -7,31 +8,40 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class StudentManagement implements Management{
+
     @Override
     public void displayEtudiants(List<Etudiant> Etudiants, Consumer<Etudiant> con) {
         for (Etudiant etudiant : Etudiants) {
-            consumer.accept(etudiant) ;
+            con.accept(etudiant) ;
         }
     }
 
     @Override
     public void displayEtudiantsByFilter(List<Etudiant> Etudiants, Predicate<Etudiant> pre, Consumer<Etudiant> con) {
-
+        for (Etudiant etudiant : Etudiants) {
+            if (pre.test(etudiant)) con.accept(etudiant) ;
+        }
     }
 
     @Override
     public String returnEtudiantsNames(List<Etudiant> Etudiants, Function<Etudiant, String> fun) {
-        return null;
+        String names ="";
+        for (Etudiant etudiant : Etudiants) {
+            names +=fun.apply(etudiant) +" ";
+        }
+        return names;
+
     }
 
     @Override
     public Etudiant createEtudiant(Supplier<Etudiant> sup) {
-        return null;
+        return sup.get();
     }
 
     @Override
     public List<Etudiant> sortEtudiantsById(List<Etudiant> Etudiants, Comparator<Etudiant> com) {
-        return null;
+        Collections.sort(Etudiants,com);
+        return Etudiants;
     }
 
     @Override
